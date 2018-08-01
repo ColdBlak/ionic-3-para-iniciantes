c4d82a262e27f011be460905020940e2df09a3e1
ColdBlak <patrick.ee.doctum@gmail.com>
2018-08-01 17:14:36 -0300

    Segundo commit

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MovieProvider } from '../../providers/movie/movie';
 
 /**
  * Generated class for the FeedPage page.
@@ -12,11 +13,29 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-feed',
   templateUrl: 'feed.html',
+  providers: [
+    MovieProvider
+  ]
 })
 export class FeedPage {
+  
+  public objeto_feed = {
+    titulo: "Patrick Felipe",
+    data: "November 5, 1955",
+    descricao:"Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy.",
+    quantidade_likes: 12,
+    quantidade_comentatios: 4,
+    data_comentario: "11h ago"
+  }
+  public lista_filmes =  new Array<any>();
+
   public nome_usuario: string = "Patrick Felipe";
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,
+    private movieProvider: MovieProvider 
+    ) {
   }
 
   public somaDoisNumeros(num1:number, num2:number): void{
@@ -26,6 +45,17 @@ export class FeedPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad FeedPage');
     //this.somaDoisNumeros(10,50);
+    this.movieProvider.getLatesMovies().subscribe(
+      data=>{
+        const response = (data as any);
+        const objeto_retorno = JSON.parse(response._body);
+        this.lista_filmes = objeto_retorno.results;
+
+        console.log(objeto_retorno);
+      }, error=>{
+        console.log(error);
+      }
+    )
   }
 
 }
